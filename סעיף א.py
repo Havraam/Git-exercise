a73c0b7c33d80bd48484a22a5d9d922242f7b76b
Erel <100906261+Havraam@users.noreply.github.com>
2024-01-31 19:14:21 +0200

    Update סעיף א.py

--- סעיף א.py
+++ סעיף א.py
@@ -1,11 +1,15 @@
 class Analog_To_Digital_Converter:
     analog_value = 0
-    def __init__(self, analog_value):
+    max_voltage = 5
+    number_of_bits = 10
+    def __init__(self, analog_value, max_voltage , number_of_bits):
         self.analog_value = analog_value
+        self.max_voltage = max_voltage
+        self.number_of_bits = number_of_bits
 
     def Analog_To_Digital(self):
-        analog_value_precent = self.analog_value/5
-        portion1024 = int(1023*analog_value_precent)
+        analog_value_precent = self.analog_value/self.max_voltage
+        portion1024 = int((2**self.number_of_bits-1)*analog_value_precent)
         print(portion1024)
         binary_num = bin(portion1024)
         print (binary_num[2:])
